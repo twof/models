b9dd309fd7bcaa6d271358e9802b29c2069e0cc7
Alex Reilly <areilly@yelp.com>
2019-11-28 10:08:12 -0800

    Package updated to 4.2

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "CloudModels",
     dependencies: [
         // Swift models, relationships, and querying for NoSQL and SQL databases.
-        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
+        .package(url: "https://github.com/vapor/fluent.git", from: "2.0.0"),
 
         // JSON wrapper around Node.
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2)
+        .package(url: "https://github.com/vapor/json.git", from: "2.0.0")
     ]
 )
