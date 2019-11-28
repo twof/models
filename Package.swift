8ffb4b6f4ba11fdc9349712a7f91f93ac0118196
Alex Reilly <areilly@yelp.com>
2019-11-28 10:40:53 -0800

    add product

--- Package.swift
+++ Package.swift
@@ -3,13 +3,19 @@ import PackageDescription
 
 let package = Package(
     name: "CloudModels",
+    products: [
+        .library(
+            name: "CloudModels",
+            targets: ["CloudModels"]
+        ),
+    ],
     dependencies: [
         // Swift models, relationships, and querying for NoSQL and SQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "2.0.0"),
 
         // JSON wrapper around Node.
         .package(url: "https://github.com/vapor/json.git", from: "2.0.0")
-    ], targets:  [
-      .target(name: "CloudModels", dependencies: ["Fluent", "JSON"])
+    ], targets: [
+        .target(name: "CloudModels", dependencies: ["Fluent", "JSON"])
     ]
 )
